da9ac3931e9649555e069a41a0388ca9c28218ef
Phuc Thinh <25023404@vnu.edu.vn>
2026-05-10 23:12:49 +0700

    Fix intentional logic error: revert EXPRESS surcharge

--- src/main/java/com/lab/ShippingCalculator.java
+++ src/main/java/com/lab/ShippingCalculator.java
@@ -6,7 +6,7 @@ public class ShippingCalculator {
         if (weight <= 0) {
             throw new IllegalArgumentException("Weight must be positive");
         }
-        if (type.equals("EXPRESS")) return weight * 5000 + 10000;
+        if (type.equals("EXPRESS")) return weight * 5000 + 20000;
         if (type.equals("STANDARD")) return weight * 3000;
         throw new IllegalArgumentException("Unknown type: " + type);
     }
